34df7016ee1b2e2e3f72c3498e46a70bceb68050
Dacio Romero <dacioromero@gmail.com>
2019-01-18 01:05:51 -0800

    Enable dotenv, fix Procfile

--- Code/app.py
+++ Code/app.py
@@ -4,13 +4,16 @@ import flask
 import random
 import markov
 import boto3
+import dotenv
+
 
 def create_app():
     app = flask.Flask(__name__)
+    dotenv.load_dotenv()
 
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    S3_BUCKET = os.environ.get('S3_BUCKET')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    S3_BUCKET = os.getenv('S3_BUCKET')
 
     if ((AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_BUCKET) is not (None, None, None)):
         print('Reading from AWS')
